0703d5db8927a140779a68bef7425f65dadcaa24
Sanya Malhotra <101451589+Sanya-Malhotra@users.noreply.github.com>
2024-04-24 20:43:50 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main
 public static void main (String args [] )
 {
 	System.out.print("hello WORLD");
+	System.out.print("hello WORLDDDDDDDDDDDDDDDDDDDDD");
 }
 }
 //static use krne  se compiler ko pta chalega ki ye vala function sbse pehle run krega
